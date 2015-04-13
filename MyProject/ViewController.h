3b4f9725ce7bca06e46a598181e3773baf5dd646
arwin <admin@Arwins-MacBook-Pro.local>
2015-04-13 14:28:02 +0530

    Initial Commit

---
+++ MyProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyProject
+//
+//  Created by arwin on 13/04/15.
+//  Copyright (c) 2015 MyOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyProject
+//
+//  Created by arwin on 13/04/15.
+//  Copyright (c) 2015 MyOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
